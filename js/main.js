97735875105b0087a26c7870f9d33ca773055ac6
Vivek Bharti <1vivekbharti@gmail.com>
2024-11-16 20:27:24 +0530

    html css javascript

---
+++ js/main.js
@@ -0,0 +1,60 @@
+import headerFunc from "./header.js"
+import productFunc from "./product.js"
+import searchFunc from "./search.js"
+
+//! add product to localstorage start
+
+(async function () {
+
+    const products = await fetch("js/data.json") //get data
+    const data = await products.json() // to json
+
+
+    data ? localStorage.setItem("products", JSON.stringify(data)) : [] // json to string
+    productFunc(data)
+    searchFunc(data)
+
+}
+)()
+
+//! add product to localstorage end
+
+
+
+//! add cartItem to localstorage start
+
+const cartItem = document.querySelector(".header-cart-count")
+
+cartItem.innerHTML = localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart")).length
+    : "0"
+
+//! add cartItem to localstorage end
+
+
+//! modal dialog start
+
+const modal = document.querySelector(".modal-dialog")
+const modalContent = document.querySelector(".modal-dialog .modal-content")
+const btnModalClose = document.querySelector(".modal-dialog .modal-close")
+
+
+btnModalClose.addEventListener("click", () => {
+    modal.classList.remove("show")
+})
+
+
+document.addEventListener("click", (e) => {
+    if (!e.composedPath().includes(modalContent)) {
+        modal.classList.remove("show")
+    }
+})
+
+
+setTimeout(() => {
+    modal.classList.add("show")
+}, 3000)
+
+
+
+//! modal dialog end
